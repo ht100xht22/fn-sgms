6a753800870938c58630646d88127d104d262520
Arif Jehda-Oh <25460850+arifjehoh@users.noreply.github.com>
2022-11-08 21:32:18 +0100

    #10 find student detail (#13)
    * Ignore .iml file
    
    * Base function for finding one student
    
    * find student by id

---
+++ stundents-info/src/main/java/arifjehoh/open/application/Application.java
@@ -0,0 +1,34 @@
+package arifjehoh.open.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private final ApplicationService service;
+
+    public Application(ApplicationService service) {
+        this.service = service;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public Function<Long, ResponseEntity<Student>> function() {
+        return id -> {
+            logger.info("Function have been made. Value: {}", id);
+            return service.findById(id);
+        };
+    }
+}
