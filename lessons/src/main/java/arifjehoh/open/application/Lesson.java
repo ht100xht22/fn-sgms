3cbde15137fb60f14bcc6465e4b393afa99d70aa
Arif Jehda-Oh <arifjeh.oh@gmail.com>
2022-11-09 23:20:28 +0100

    funciton for fetching all available lesson (#19)

---
+++ lessons/src/main/java/arifjehoh/open/application/Lesson.java
@@ -0,0 +1,108 @@
+package arifjehoh.open.application;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "lesson")
+public class Lesson {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private LocalDate from;
+    private LocalTime time;
+    private String level;
+    private String instrument;
+    private String genre;
+    private String type;
+    @OneToMany(targetEntity = Student.class)
+    private List<Student> students;
+    private int maxCapacity;
+    private int minCapacity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalDate from) {
+        this.from = from;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public int getMinCapacity() {
+        return minCapacity;
+    }
+
+    public void setMinCapacity(int minCapacity) {
+        this.minCapacity = minCapacity;
+    }
+
+    public boolean isBookAble() {
+        return students.size() < maxCapacity;
+    }
+}
